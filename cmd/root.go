308c79363a3ef10bdf799bbef1be2ea8aba38d45
Ziad Elshahawy <ziadelshahawygit@gmail.com>
2024-12-26 21:50:21 +0900

    added dictionary creation

--- cmd/root.go
+++ cmd/root.go
@@ -2,12 +2,16 @@ package cmd
 
 import (
 	"fmt"
+
+	"github.com/zelshahawy/go-lzw/internal"
 )
 
 func StartEncoding(fileName string) {
 	fmt.Printf("Encoding has started on %v\n", fileName)
+	internal.EncodeFile()
 }
 
 func StartDecoding(fileName string) {
 	fmt.Printf("Decoding has started on %v\n", fileName)
+	internal.DecodeFile()
 }
